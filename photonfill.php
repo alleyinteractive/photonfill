662fe79a049a8e31fc30c583b25d55327a1121ea
Jared Cobb <github@jaredcobb.com>
2016-01-28 08:41:58 +0000

    version bump

--- photonfill.php
+++ photonfill.php
@@ -2,14 +2,14 @@
 /**
  * @package Photonfill
  * @subpackage Plugin
- * @version 0.1.6
+ * @version 0.1.7
  */
 /*
 Plugin Name: Photonfill
 Plugin URI: http://github.com/willgladstone/photonfill
 Description: Integrate Jetpack Photon and Picturefill into WP images
 Author: Will Gladstone
-Version: 0.1.6
+Version: 0.1.7
 Author URI: http://www.alleyinteractive.com/
 */
 
