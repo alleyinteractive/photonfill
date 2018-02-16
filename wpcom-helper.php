9f74740645560f66f971b44a37f5c72d6189d54b
David Binovec <david.binovec@automattic.com>
2018-02-16 13:16:41 +0000

    Adding wpcom-helper.php file
    As on the WordPress.com VIP platform the recommended way for activating the plugin is via theme's functions.php, the `photonfill_init` function hooked to `plugins_loaded` does not get triggered, as it's too late.
    
    The wpcom-helper.php file gets included right after the plugin gets loaded. It currently contians only the call to `photonfill_init()` for setting up the plugin.

---
+++ wpcom-helper.php
@@ -0,0 +1,11 @@
+<?php
+/*
+ * Helper file which gets included during the `wpcom_vip_load_plugin` activation
+ * from theme's functions.php on the WordPress.com VIP platform.
+ */
+
+/*
+ * Call photonfill_init function manually, as it was supposed to get called
+ * on plugins_loaded hook, which already fired before we included plugin's files.
+ */
+photonfill_init();
