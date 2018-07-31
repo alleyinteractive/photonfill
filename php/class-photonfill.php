f7af8b52b6148f39fddb8f4986f1ed1878b177d0
Erick Hitter <git-contrib@ethitter.com>
2018-07-30 17:45:42 -0700

    Correct warning arising from calling wrong function

--- php/class-photonfill.php
+++ php/class-photonfill.php
@@ -693,15 +693,12 @@ if ( ! class_exists( 'Photonfill' ) ) {
 		 */
 		function ajax_get_img_object() {
 			check_ajax_referer( 'photonfill_get_img_object', 'nonce' );
-			// Disable PHPCS warning on inspecting super global.
-			// @codingStandardsIgnoreStart
 			if ( ! empty( $_POST['attachment'] ) ) {
 				$attachment_id = absint( $_POST['attachment'] );
-				echo wp_kses( $this->get_attachment_image( $attachment_id, 'full', array(
+				echo wp_kses_post( $this->get_attachment_image( $attachment_id, 'full', array(
 					'style' => 'max-width:100%',
 				) ) );
 			}
-			// @codingStandardsIgnoreEnd
 			exit();
 		}
 
